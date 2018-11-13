9a6eca65d5312fe0a3fee704e5cf1c202de2ed08
Charles <Chris.Lyle101@gmail.com>
2018-11-13 17:05:14 +0800

    注册，激活，登陆，退出，类视图，邮件发送

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import url
+from user.views import RegisterView, ActiveView, LoginView
 
 urlpatterns = [
+    # url(r'^register$', views.register, name='register'), # 注册
+    # url(r'^register_handle$', views.register_handle, name='register_handle'), # 注册处理
 
+    url(r'^register$', RegisterView.as_view(), name='register'),  # 注册
+    url(r'^active/(?P<token>.*)$', ActiveView.as_view(), name='active'),  # 用户激活
+    url(r'^login$', LoginView.as_view(), name='login'),  # 登录
 ]
--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -122,6 +122,17 @@ TINYMCE_DEFAULT_CONFIG = {
     'height': 400,
 }
 
+# 发送邮件配置
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.163.com'
+EMAIL_PORT = 25
+# 发送邮件的邮箱
+EMAIL_HOST_USER = 'skyli1012@163.com'
+# 在邮箱中设置的客户端授权密码
+EMAIL_HOST_PASSWORD = os.environ['email_auth']
+# 收件人看到的发件人
+EMAIL_FROM = '天天生鲜 <skyli1012@163.com>'
 
 # Django的缓存配置
 CACHES = {
