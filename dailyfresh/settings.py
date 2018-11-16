9830f113a8ad88800d8fbd0319cca46d18cba027
Charles <Chris.Lyle101@gmail.com>
2018-11-16 18:10:31 +0800

    自定义文件存储类FDFSStorage--通过fastDFS+Nignx上传和存储图片

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -150,4 +150,13 @@ SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
 
 # 配置登录url地址
-LOGIN_URL='/user/login' # /accounts/login
+LOGIN_URL = '/user/login'  # /accounts/login?next=/user
+
+# 设置Django的文件存储类
+DEFAULT_FILE_STORAGE = 'utils.fdfs.storage.FDFSStorage'
+
+# 设置fdfs使用的client.conf文件路径
+FDFS_CLIENT_CONF = './utils/fdfs/client.conf'
+
+# 设置fdfs存储服务器上nginx的IP和端口号
+FDFS_URL = 'http://localhost:8888/'
