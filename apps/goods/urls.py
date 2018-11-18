1800479157864c9fc535c025aa529bb9a5752510
Charles <Chris.Lyle101@gmail.com>
2018-11-18 15:13:03 +0800

    配置nginx提交index.html静态页面

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from goods.views import IndexView
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index'), # 首页
+    url(r'^index$', IndexView.as_view(), name='index'), # 首页
 ]
