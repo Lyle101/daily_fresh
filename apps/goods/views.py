2309c770a9cc106431ed0d80fd1f4ce97bd7bd3b
Charles <Chris.Lyle101@gmail.com>
2018-11-18 20:22:54 +0800

    商品详情信息的获取和显示

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.views.generic import View
 from django.core.cache import cache
-from goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
+from goods.models import GoodsType, GoodsSKU, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
 from django_redis import get_redis_connection
+from order.models import OrderGoods
 # Create your views here.
 
 
@@ -59,3 +62,43 @@ class IndexView(View):
 
         # 使用模板
         return render(request, 'index.html', context)
+
+
+# /goods/商品id
+class DetailView(View):
+    '''详情页'''
+
+    def get(self, request, goods_id):
+        '''显示详情页'''
+        try:
+            sku = GoodsSKU.objects.get(id=goods_id)
+        except GoodsSKU.DoesNotExist:
+            # 商品不存在
+            return redirect(reverse('goods:index'))
+
+        # 获取商品的分类信息
+        types = GoodsType.objects.all()
+
+        # 获取商品的评论信息
+        sku_orders = OrderGoods.objects.filter(sku=sku).exclude(comment='')
+
+        # 获取新品信息
+        new_skus = GoodsSKU.objects.filter(type=sku.type).order_by('-create_time')
+
+        # 获取用户购物车中商品的数目
+        user = request.user
+        cart_count = 0
+        if user.is_authenticated:
+            # 用户已登录
+            conn = get_redis_connection('default')
+            cart_key = 'cart_%d' % user.id
+            cart_count = conn.hlen(cart_key)
+
+        # 组织模板上下文
+        context = {'sku': sku, 'types': types,
+                   'sku_orders': sku_orders,
+                   'new_skus': new_skus,
+                   'cart_count': cart_count}
+
+        # 使用模板
+        return render(request, 'detail.html', context)
