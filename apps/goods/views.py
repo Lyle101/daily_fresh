755b4088ca9c47e20047f42a3dd9bf5fce59c8ad
Charles <Chris.Lyle101@gmail.com>
2018-11-19 15:50:27 +0800

    进行页码的控制，页面上最多显示5个页码

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -168,7 +168,19 @@ class ListView(View):
         # 获取第page页的Page实例对象
         skus_page = paginator.page(page)
 
-        # todo: 进行页码的控制，页面上最多显示5个页码
+        # 1.总页数小于5页，页面上显示所有页码
+        # 2.如果当前页是前3页，显示1-5页
+        # 3.如果当前页是后3页，显示后5页
+        # 4.其他情况，显示当前页的前2页，当前页，当前页的后2页
+        num_pages = paginator.num_pages
+        if num_pages < 5:
+            pages = range(1, num_pages + 1)
+        elif page <= 3:
+            pages = range(1, 6)
+        elif num_pages - page <= 2:
+            pages = range(num_pages - 4, num_pages + 1)
+        else:
+            pages = range(page - 2, page + 3)
 
         # 获取新品信息
         new_skus = GoodsSKU.objects.filter(type=type).order_by('-create_time')[:2]
@@ -187,6 +199,7 @@ class ListView(View):
                    'skus_page': skus_page,
                    'new_skus': new_skus,
                    'cart_count': cart_count,
+                   'pages': pages,
                    'sort': sort}
 
         # 使用模板
