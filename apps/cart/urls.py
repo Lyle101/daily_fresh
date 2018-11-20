10fdac35db60d9e0bf8dc8f3907f6d313255fd7a
Charles <Chris.Lyle101@gmail.com>
2018-11-20 17:41:13 +0800

    购物车记录添加-后台View

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
+from cart.views import CartAddView
 
 urlpatterns = [
-
+    url(r'^add$', CartAddView.as_view(), name='add'), # 购物车记录添加
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,3 +1,72 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import JsonResponse
+
+from goods.models import GoodsSKU
+from django_redis import get_redis_connection
 
 # Create your views here.
+# 添加商品到购物车:
+# 1）请求方式，采用ajax post
+# 如果涉及到数据的修改(新增，更新，删除), 采用post
+# 如果只涉及到数据的获取，采用get
+# 2) 传递参数: 商品id(sku_id) 商品数量(count)
+
+
+# ajax发起的请求都在后台，在浏览器中看不到效果
+# /cart/add
+class CartAddView(View):
+    '''购物车记录添加'''
+
+    def post(self, request):
+        '''购物车记录添加'''
+        user = request.user
+        if not user.is_authenticated():
+            # 用户未登录
+            return JsonResponse({'res': 0, 'errmsg': '请先登录'})
+
+        # 接收数据
+        sku_id = request.POST.get('sku_id')
+        count = request.POST.get('count')
+
+        # 数据校验
+        if not all([sku_id, count]):
+            return JsonResponse({'res': 1, 'errmsg': '数据不完整'})
+
+        # 校验添加的商品数量
+        try:
+            count = int(count)
+        except Exception as e:
+            # 数目出错
+            return JsonResponse({'res': 2, 'errmsg': '商品数目出错'})
+
+        # 校验商品是否存在
+        try:
+            sku = GoodsSKU.objects.get(id=sku_id)
+        except GoodsSKU.DoesNotExist:
+            # 商品不存在
+            return JsonResponse({'res': 3, 'errmsg': '商品不存在'})
+
+        # 业务处理:添加购物车记录
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d' % user.id
+        # 先尝试获取sku_id的值 -> hget cart_key 属性
+        # 如果sku_id在hash中不存在，hget返回None
+        cart_count = conn.hget(cart_key, sku_id)
+        if cart_count:
+            # 累加购物车中商品的数目
+            count += int(cart_count)
+
+        # 校验商品的库存
+        if count > sku.stock:
+            return JsonResponse({'res': 4, 'errmsg': '商品库存不足'})
+
+        # 设置hash中sku_id对应的值
+        # hset->如果sku_id已经存在，更新数据， 如果sku_id不存在，添加数据
+        conn.hset(cart_key, sku_id, count)
+
+        # 计算用户购物车商品的条目数
+        total_count = conn.hlen(cart_key)
+
+        # 返回应答
+        return JsonResponse({'res': 5, 'total_count': total_count, 'message': '添加成功'})
