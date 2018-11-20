7daef19aeeb7527870222788e499bc0b62ac4593
Charles <Chris.Lyle101@gmail.com>
2018-11-20 16:27:40 +0800

    更改索引分词方式 -- jieba
    * 安装中文分词包 jieba
    
    ```sh
    pip install jieba
    ```
    
    * Anaconda/虚拟环境下的haystack目录,创建ChineseAnalyzer.py文件
    
    ```sh
    cd /Users/charles/anaconda3/envs/mydjango_env/lib/python3.7/site-packages/haystack/backends/
    vim ChineseAnalyzer.py
    ```
    
    ```py
    import jieba
    from whoosh.analysis import Tokenizer, Token
    
    class ChineseTokenizer(Tokenizer):
        def __call__(self, value, positions=False, chars=False,
                     keeporiginal=False, removestops=True,
                     start_pos=0, start_char=0, mode='', **kwargs):
            t = Token(positions, chars, removestops=removestops, mode=mode,
                      **kwargs)
            seglist = jieba.cut(value, cut_all=True)
            for w in seglist:
                t.original = t.text = w
                t.boost = 1.0
                if positions:
                    t.pos = start_pos + value.find(w)
                if chars:
                    t.startchar = start_char + value.find(w)
                    t.endchar = start_char + value.find(w) + len(w)
                yield t
    
    def ChineseAnalyzer():
        return ChineseTokenizer()
    
    ```
    
    * 复制whoosh_backend.py文件
    
        cp whoosh_backend.py whoosh_cn_backend.py
    
    * 打开复制出来的新文件，引入中文分析类，内部采用jieba分词。
    
        from .ChineseAnalyzer import ChineseAnalyzer
    
    * 更改词语分析类:
    
        查找
        analyzer=StemmingAnalyzer()
        改为
        analyzer=ChineseAnalyzer()
    
    * 重新建立索引
    
        python manage.py rebuild_index

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -165,8 +165,8 @@ FDFS_URL = 'http://localhost:8888/'
 HAYSTACK_CONNECTIONS = {
     'default': {
         # 使用whoosh引擎
-        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-        # 'ENGINE': 'haystack.backends.whoosh_cn_backend.WhooshEngine',
+        # 'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'ENGINE': 'haystack.backends.whoosh_cn_backend.WhooshEngine',
         # 索引文件路径
         'PATH': os.path.join(BASE_DIR, 'whoosh_index'),
     }
