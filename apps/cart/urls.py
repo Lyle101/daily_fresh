5e41a75e9fafe990ae998666b75c8faf4ab93dc0
Charles <Chris.Lyle101@gmail.com>
2018-11-21 18:31:24 +0800

    购物车记录更新-后台CartUpdateView
    ```py
    # 接收数据
    sku_id = request.POST.get('sku_id')
    count = request.POST.get('count')
    ```

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from cart.views import CartAddView, CartInfoView
+from cart.views import CartAddView, CartInfoView, CartUpdateView
 
 urlpatterns = [
     url(r'^add$', CartAddView.as_view(), name='add'),  # 购物车记录添加
     url(r'^$', CartInfoView.as_view(), name='show'),  # 购物车页面显示
+    url(r'^update$', CartUpdateView.as_view(), name='update'),  # 购物车记录更新
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -115,3 +115,54 @@ class CartInfoView(LoginRequiredMixin, View):
 
         # 使用模板
         return render(request, 'cart.html', context)
+
+
+# 更新购物车记录
+# 采用ajax post请求
+# 前端需要传递的参数:商品id(sku_id) 更新的商品数量(count)
+# /cart/update
+class CartUpdateView(View):
+    '''购物车记录更新'''
+
+    def post(self, request):
+        '''购物车记录更新'''
+        user = request.user
+        if not user.is_authenticated():
+            # 用户未登录
+            return JsonResponse({'res': 0, 'errmsg': '请先登录'})
+
+        # 接收数据
+        sku_id = request.POST.get('sku_id')
+        count = request.POST.get('count')
+
+        # 数据校验
+        if not all([sku_id, count]):
+            return JsonResponse({'res': 1, 'errmsg': '数据不完整'})
+
+        # 校验添加的商品数量
+        try:
+            count = int(count)
+        except Exception as e:
+            # 数目出错
+            return JsonResponse({'res': 2, 'errmsg': '商品数目出错'})
+
+        # 校验商品是否存在
+        try:
+            sku = GoodsSKU.objects.get(id=sku_id)
+        except GoodsSKU.DoesNotExist:
+            # 商品不存在
+            return JsonResponse({'res': 3, 'errmsg': '商品不存在'})
+
+        # 业务处理:更新购物车记录
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d' % user.id
+
+        # 校验商品的库存
+        if count > sku.stock:
+            return JsonResponse({'res': 4, 'errmsg': '商品库存不足'})
+
+        # 更新
+        conn.hset(cart_key, sku_id, count)
+
+        # 返回应答
+        return JsonResponse({'res': 5, 'message': '更新成功'})
