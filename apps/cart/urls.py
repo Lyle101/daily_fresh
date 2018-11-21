51c10dccf88422e1b11eb69a2ff989b50eedff81
Charles <Chris.Lyle101@gmail.com>
2018-11-21 14:47:33 +0800

    购物车页面cart.html显示

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from cart.views import CartAddView
+from cart.views import CartAddView, CartInfoView
 
 urlpatterns = [
-    url(r'^add$', CartAddView.as_view(), name='add'), # 购物车记录添加
+    url(r'^add$', CartAddView.as_view(), name='add'),  # 购物车记录添加
+    url(r'^$', CartInfoView.as_view(), name='show'),  # 购物车页面显示
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 
 from goods.models import GoodsSKU
 from django_redis import get_redis_connection
+from utils.mixin import LoginRequiredMixin
 
 # Create your views here.
 # 添加商品到购物车:
@@ -70,3 +71,47 @@ class CartAddView(View):
 
         # 返回应答
         return JsonResponse({'res': 5, 'total_count': total_count, 'message': '添加成功'})
+
+
+# /cart/
+class CartInfoView(LoginRequiredMixin, View):
+    '''购物车页面显示'''
+
+    def get(self, request):
+        '''显示'''
+        # 获取登录的用户
+        user = request.user
+        # 获取用户购物车中商品的信息
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d' % user.id
+        # {'商品id':商品数量}
+        cart_dict = conn.hgetall(cart_key)
+
+        skus = []
+        # 保存用户购物车中商品的总数目和总价格
+        total_count = 0
+        total_price = 0
+        # 遍历获取商品的信息
+        for sku_id, count in cart_dict.items():
+            # 根据商品的id获取商品的信息
+            sku = GoodsSKU.objects.get(id=sku_id)
+            # 计算商品的小计
+            amount = sku.price * int(count)
+            # 动态给sku对象增加一个属性amount, 保存商品的小计
+            sku.amount = amount
+            # 动态给sku对象增加一个属性count, 保存购物车中对应商品的数量
+            sku.count = int(count)
+            # 添加
+            skus.append(sku)
+
+            # 累加计算商品的总数目和总价格
+            total_count += int(count)
+            total_price += amount
+
+        # 组织上下文
+        context = {'total_count': total_count,
+                   'total_price': total_price,
+                   'skus': skus}
+
+        # 使用模板
+        return render(request, 'cart.html', context)
