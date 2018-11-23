a3d3e28fd5360bc547576f9381d3370c0a78bc15
Charles <Chris.Lyle101@gmail.com>
2018-11-23 19:28:35 +0800

    订单支付-支付宝(沙箱)

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from order.views import OrderPlaceView, OrderCommitView
+from order.views import OrderPlaceView, OrderCommitView, OrderPayView
 
 urlpatterns = [
     url(r'^place$', OrderPlaceView.as_view(), name='place'),  # 提交订单页面显示
     url(r'^commit$', OrderCommitView.as_view(), name='commit'),  # 订单创建
+    url(r'^pay$', OrderPayView.as_view(), name='pay'),  # 订单支付
 ]
